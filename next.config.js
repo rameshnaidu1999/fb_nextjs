d181e90a42f9ba163f8d3b49fd1960b864c8a669
rameshnaidu1999 <kasularameshnaidu@gmail.com>
2021-05-27 10:58:47 +0530

    second commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            "links.papareact.com",
+            "platform_lookaside.fbsbx.com",
+            "firebasestorage.googleapis.com"
+        ]
+    }
+}
